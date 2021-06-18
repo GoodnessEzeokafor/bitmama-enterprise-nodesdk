2bac5280247616073bdd67a4ba1805f46a67d755
Goodness Ezeokafor <gootech442@gmail.com>
2021-06-18 19:54:12 +0100

    setup rate and get webhook endpoint ezamples

---
+++ src/examples/getWebhook.ts
@@ -0,0 +1,18 @@
+/**
+ *  Get rate between currencies 
+*/
+
+
+
+import BitmamaEnterprise from "../index"
+import { TEST_API_TOKEN, TEST_ENV } from "./setup";
+
+
+
+// setup credentials
+const bitmama = BitmamaEnterprise(TEST_API_TOKEN, TEST_ENV);
+bitmama.getWebhook()
+  .then((res: any) => console.log(res))
+  .catch((e: any) => console.log(e))
+  
+
