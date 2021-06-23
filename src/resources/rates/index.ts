f963d7135df9bd559a7a318387fdb2cada3ff9c9
Goodness Ezeokafor <gootech442@gmail.com>
2021-06-23 04:35:29 +0100

    setting up btc

--- src/resources/rates/index.ts
+++ src/resources/rates/index.ts
@@ -1,5 +1,6 @@
 import { Ticker } from "types";
 import axiosLib from "utils/axios";
+import { cryptoRate } from "utils/swapRate";
 
 // const root = "/address"
 const rates = {
@@ -33,12 +34,83 @@ const rates = {
     ];
     return tickers;
   },
-  EthToBtcTicker: () => {
-      return ""
+  // ethereum
+  EthNGNRate: async (url: string, token: string) => {
+    const ticker: Ticker = "ethngn";
+    return await axiosLib.getRate(url, token, ticker);
   },
-  EthToBtcRate: async() => {
-    
+  EthGHSRate: async (url: string, token: string) => {
+    const ticker: Ticker = "ethghs";
+    return await axiosLib.getRate(url, token, ticker);
   },
+  EthBtcRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "eth", "btc");
+    return rate;
+  },
+  EthCeloRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "eth", "celo");
+    return rate;
+  },
+  EthXrpRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "eth", "xrp");
+    return rate;
+  },
+  EthXlmRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "eth", "xlm");
+    return rate;
+  },
+  EthCusdRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "eth", "cusd");
+    return rate;
+  },
+  EthCeurRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "eth", "ceur");
+    return rate;
+  },
+
+  // bitcoin
+  BtcNGNRate: async (url: string, token: string) => {
+    const ticker: Ticker = "btcngn";
+    return await axiosLib.getRate(url, token, ticker);
+  },
+  BtcGHSRate: async (url: string, token: string) => {
+    const ticker: Ticker = "btcghs";
+    return await axiosLib.getRate(url, token, ticker);
+  },
+  BtcEthRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "btc", "eth");
+    return rate;
+  },
+  BtcCeloRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "btc", "celo");
+    return rate;
+  },
+  BtcXrpRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "btc", "xrp");
+    return rate;
+  },
+  BtcXlmRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "btc", "eth");
+    return rate;
+  },
+  BtcCusdRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "btc", "cusd");
+    return rate;
+  },
+  BtcCeurRate: async (url: string, token: string) => {
+    const rate = cryptoRate(url, token, "btc", "ceur");
+    return rate;
+  },
+  // celo
+
+  // ripple
+
+  // stellar
+
+  // cusd
+
+  // ceur
+
 
 }
 export default rates
